9fed82c15215df63161b2a23338003c3156a1e90
Suhani1102 <sandhusuhani1102@gmail.com>
2022-04-11 14:11:57 +0530

    text alignment

--- src/components/GroceryItem.jsx
+++ src/components/GroceryItem.jsx
@@ -1,5 +1,6 @@
 // reusable card component
 import styles from "./GroceryItem.module.css";
+//import CartButton from "./CartButton";
 
 const GroceryItem = (props) => {
   const { title, imgURL, mrp, sellingPrice } = props;
@@ -11,6 +12,7 @@ const GroceryItem = (props) => {
         <span className={styles.sellPrice}>{sellingPrice} </span>
         M.R.P.<span className={styles.actualPrice}>{mrp}</span>
       </div>
+      {/* <CartButton /> */}
     </div>
   );
 };
--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import data from "../data.json";
 import GroceryItem from "./GroceryItem";
-//import CartButton from "./CartButton";
 import styles from "./GroceryItem.module.css";
 
 const GroceryDetails = () => {
